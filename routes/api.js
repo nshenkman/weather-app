6c48342e673ca45e070aec6a32e91d0425a93b27
nshenkman <nikita.shenkman@gmail.com>
2017-03-16 22:27:31 -0400

    working state

---
+++ routes/api.js
@@ -0,0 +1,108 @@
+/*
+ * Serve JSON to our AngularJS client
+ */
+var connection = require('../app').getConnection;
+var async = require('async');
+var request = require('request');
+
+
+exports.account  =
+  {
+    emailCheck : function(req, res) {
+      var email = decodeURI(req.query.email);
+      async.auto({
+        connection : connection,
+        getAccount : ['connection', function(results, callback) {
+          var connection = results.connection;
+          connection.query('SELECT email, location_link as locationLink, location_name as locationName FROM weather_app.accounts WHERE email = ? LIMIT 1', email, function(err, account) {
+            connection.release();
+            callback(err, account)
+          })
+        }]
+      }, function(err, results) {
+        if (err) {
+          console.log(err);
+          res.send(500)
+        } else if (results.getAccount.length == 1) {
+          res.send(400)
+        } else {
+          res.send(200)
+        }
+      })
+    },
+    post : function(req, res) {
+      var account = {};
+      account.email = req.body.email;
+      account.location_link = req.body.locationLink;
+      account.location_name = req.body.locationName;
+      async.auto({
+        connection : connection,
+        createAccount : ['connection', function(results, callback) {
+          var connection = results.connection;
+          connection.query('INSERT INTO weather_app.accounts SET ?', account, function(err, results) {
+            connection.release();
+            callback(err, results)
+          })
+        }]
+      }, function(err) {
+        if (err) {
+          console.log(err);
+          res.send(500)
+        } else {
+          res.send(200)
+        }
+      })
+    }
+  };
+
+exports.wunderground =
+  {
+    getCities : function(req, res) {
+      var qs = req.query;
+      async.auto({
+        getCities : function(callback) {
+          qs.c = 'US';
+
+          var options = {
+            uri : 'http://autocomplete.wunderground.com/aq',
+            method : 'GET',
+            qs : qs
+          };
+
+          request(options, function(err, response, body) {
+            callback(err, body)
+          })
+        }
+      }, function(err, results){
+        if (err) {
+          res.send(500)
+        } else {
+          var cities = results.getCities;
+          cities = JSON.parse(cities).RESULTS;
+          res.json(cities)
+        }
+      })
+    },
+    get : function(type, urlRoute, callback) {
+      async.auto({
+        request : function(callback) {
+          var options = {
+            url : 'http://api.wunderground.com/api/142236cd1b398b92/'+ type + urlRoute + '.json',
+            method : 'GET'
+          };
+
+          request(options, function(err, response, body) {
+            callback(err, body)
+          })
+        }
+      }, function(err, results) {
+        if (err) {
+          callback(err, {})
+        } else {
+          var apiResponse = results.request;
+          apiResponse = JSON.parse(apiResponse);
+          callback(null, apiResponse)
+        }
+      })
+    }
+  };
---
+++ sendEmails.js
@@ -0,0 +1,78 @@
+'use strict';
+exports.sendEmails = function(email, pass, callback){
+  var connection = require('./app').getConnection;
+  var api = require('./routes/api');
+  var nodemailer = require('nodemailer');
+  var async = require('async');
+  var mustache = require('mustache');
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: pass
+    }
+  });
+
+  var  mailOptions = {
+    from: 'Nikita Shenkman'
+  };
+
+  var weatherTemplates = {
+    high : {subject : "It's nice out! Enjoy a discount on us.", template : '<p>In {{location_name}} the weather is {{forecast.temp}} degrees and {{forecast.weather}}.</p>'},
+    low : {subject : "Not so nice out? That's okay, enjoy a discount on us.", template : '<p>In {{location_name}} the weather is {{forecast.temp}} degrees and {{forecast.weather}}.</p>'},
+    normal : {subject : "Enjoy a discount on us.", template : '<p>In {{location_name}} the weather is {{forecast.temp}} degrees and {{forecast.weather}}.</p>'}
+  };
+
+  async.auto(
+    {
+      connection : connection,
+      findAccounts : ['connection', function(results, callback) {
+        connection = results.connection;
+        connection.query('SELECT email, location_link, location_name from weather_app.accounts', function(err, results) {
+          connection.release();
+          callback(err, results)
+        });
+      }],
+      getWeatherData : ['findAccounts', function(results, callback) {
+        var accounts = results.findAccounts;
+        async.map(accounts, function(account, callback) {
+          async.parallel({
+            averageForecast : async.apply(api.wunderground.get, 'almanac', account.location_link),
+            currentForecast : async.apply(api.wunderground.get, 'conditions', account.location_link)
+          }, function(err, weatherData) {
+            if (err) {
+              callback()
+            } else {
+              var forecast = {};
+              var averageForecast = weatherData.averageForecast.almanac;
+              var currentForecast = weatherData.currentForecast.current_observation;
+              forecast.averageTemp = (Number(averageForecast.temp_high.normal.F) + Number(averageForecast.temp_low.normal.F)) / 2;
+              forecast.weather = currentForecast.weather;
+              forecast.temp = currentForecast.temp_f;
+              account.forecast = forecast;
+              callback(null, account)
+            }
+          })
+        }, callback)
+      }],
+      sendEmails : ['getWeatherData', function(results, callback) {
+        async.each(results.getWeatherData, function(accountWeather, callback){
+          if (accountWeather) {
+            var weatherType = 'normal';
+            var weatherDifference = accountWeather.forecast.temp - accountWeather.forecast.averageTemp;
+            var weather = accountWeather.forecast.temp;
+            if (weatherDifference >= 5 || weather == 'Sunny') {
+              weatherType = 'high'
+            } else if (weatherDifference <= -5 || weather == 'Rain') {
+              weatherType = 'low'
+            }
+            var html = mustache.render(weatherTemplates[weatherType].template, accountWeather);
+            mailOptions.subject = weatherTemplates[weatherType].subject;
+            mailOptions.html = html;
+            mailOptions.to = accountWeather.email;
+            transporter.sendMail(mailOptions, callback)
+          }
+        }, callback)
+      }]
+    }, callback);
+};
